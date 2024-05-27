9338a9befb428ebccb2748c33ca0f7cc14867cbd
Dan Koskei <qimtaid@gmail.com>
2024-05-27 04:14:55 +0300

    complete lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,9 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Python", "Java", "C++"]
 
     def teach(self):
-        pass
+        return self.knowledge[0]
